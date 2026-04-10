a5b2a922dc2e09b90394dbf5f903c8549fcf9b24
Anibal Alvarez <aalvarez@chmw.co>
2026-04-10 08:43:42 -0400

    chore: make common config merging dynamic

--- src/Drivers/XDriver.php
+++ src/Drivers/XDriver.php
@@ -12,6 +12,11 @@ use Anibalealvarezs\ApiDriverCore\Interfaces\SeederInterface;
 
 class XDriver implements SyncDriverInterface
 {
+
+    public static function getCommonConfigKey(): ?string
+    {
+        return null;
+    }
     use HasUpdatableCredentials;
 
     private ?AuthProviderInterface $authProvider = null;
