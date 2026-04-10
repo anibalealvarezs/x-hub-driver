8f0dc5d785f40e921b604448cdd96b0f5b3fe730
Anibal Alvarez <aalvarez@chmw.co>
2026-04-10 08:12:44 -0400

    chore: update dependencies

--- src/Auth/XAuthProvider.php
+++ src/Auth/XAuthProvider.php
@@ -2,7 +2,7 @@
 
 namespace Anibalealvarezs\XHubDriver\Auth;
 
-use Anibalealvarezs\ApiDriverCore\Interfaces\AuthProviderInterface;
+use Anibalealvarezs\ApiSkeleton\Interfaces\AuthProviderInterface;
 
 class XAuthProvider implements AuthProviderInterface
 {
--- src/Drivers/XDriver.php
+++ src/Drivers/XDriver.php
@@ -3,8 +3,8 @@
 namespace Anibalealvarezs\XHubDriver\Drivers;
 
 use Anibalealvarezs\ApiDriverCore\Interfaces\SyncDriverInterface;
-use Anibalealvarezs\ApiDriverCore\Interfaces\AuthProviderInterface;
-use Anibalealvarezs\ApiDriverCore\Traits\HasUpdatableCredentials;
+use Anibalealvarezs\ApiSkeleton\Interfaces\AuthProviderInterface;
+use Anibalealvarezs\ApiSkeleton\Traits\HasUpdatableCredentials;
 use Symfony\Component\HttpFoundation\Response;
 use Psr\Log\LoggerInterface;
 use DateTime;
